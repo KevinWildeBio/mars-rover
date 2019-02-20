3cb62c01954e1240f024c6389eea335a1c1346a5
Kevin Wilde <kevin.wilde@thebioagency.com>
2019-02-20 11:26:18 +0000

    create expandable moveset

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const input = `
   LLFFFLFLFL
 `
 
-const stringToArray = ( string, seperator ) => string.split(seperator);
+//create a moveset which can easily expanded
+const moveSet = {
+  R: console.log('TURN RIGHT'),
+  L: console.log('TURN LEFT'),
+  F: console.log('MOVE FORWARDS')
+}
 
-const rover = ( position, path ) => ({
+const stringToArray = (string, seperator) => string.split(seperator);
+
+const rover = (position, path) => ({
   position: [position[0], position[1]],
   orientation: position[2],
   lost: false,
@@ -19,11 +26,16 @@ const rover = ( position, path ) => ({
 });
 
 function mission(input) {
-  console.log(input);
   const grid = stringToArray('5 3 E', ' ');
   const position = stringToArray('1 1 E', ' ');
-  const sprit = rover(position, 'RFRFRFRF');
-  console.log(sprit);
+  const spirit = rover(position, 'RFRFRFRF');
+  navigatePath(spirit);
+}
+
+function navigatePath(robot) {
+  for (var i = 0; i < robot.path.length; i++) {
+    console.log(robot.path[i]);
+  }
 }
 
 mission(input);
