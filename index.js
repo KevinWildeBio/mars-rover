7a96bba527a46c0dc2930eae21deb054ab4c56d2
Kevin Wilde <kevin.wilde@thebioagency.com>
2019-02-20 13:17:58 +0000

    adding out of bounds checking

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ LLFFFLFLFL`
 
 const smellList = [];
 const compassArray = ['N', 'E', 'S', 'W'];
-let grid;
-
-const stringToArray = (string, seperator) => string.split(seperator);
+let grid
 
 const rover = (position, path) => ({
   xLoc: parseInt(position[0], 10),
   yLoc: parseInt(position[1], 10),
   orientation: position[2],
   lost: false,
-  path: stringToArray(path, ''),
+  path: path.split(''),
   sniff: position => smellList.includes(position)
 });
 
 function setupMission(input) {
-  console.log(input.split('\n'));
-  grid = stringToArray('5 3', ' ');
-  const robots = [
-    rover(stringToArray('1 1 E', ' '), 'RFRFRFRF'),
-    rover(stringToArray('3 2 N', ' '), 'FRRFLLFFRRFLL'),
-    rover(stringToArray('0 3 W', ' '), 'LLFFFLFLFL'),
-  ];
+  const data = input.split('\n');
+  let robots = [];
+
+  grid = data[0].split(' ');
+  data.shift();
+
+  for (var i = 0; i < data.length; i += 2) {
+    robots.push(rover(data[i].split(' '), data[i+1]))
+  }
 
   beginMission(robots);
 }
@@ -41,48 +41,58 @@ function navigatePath(robot) {
   for (var i = 0; i < robot.path.length; i++) {
     // console.log('SMELL: ', robot.sniff(robot.position));
     // console.log('GRID: ', grid);
+    console.log(`${robot.xLoc} ${robot.yLoc} ${robot.orientation}`);
+    console.log(robot.path[i]);
 
-    if (robot.path[i] === 'R') {
-      let currentIndex = compassArray.indexOf(robot.orientation);
-      let newIndex = currentIndex - 1 < 0 ? compassArray.length - 1 : currentIndex - 1;
-      robot.orientation = compassArray[newIndex];
-    }
-
-    if (robot.path[i] === 'L') {
-      let currentIndex = compassArray.indexOf(robot.orientation);
-      let newIndex = currentIndex + 1 > (compassArray.length - 1) ? 0 : currentIndex + 1;
-      robot.orientation = compassArray[newIndex];
-    }
+    if (robot.path[i] === 'R') robot.orientation = pivotRight(robot);
+    if (robot.path[i] === 'L') robot.orientation = pivotLeft(robot);
 
     if (robot.path[i] === 'F') {
-      if (robot.orientation === 'N') robot.xLoc = robot.xLoc + 1;
-      if (robot.orientation === 'S') robot.xLoc = robot.xLoc - 1;
-      if (robot.orientation === 'E') robot.yLoc = robot.yLoc + 1;
-      if (robot.orientation === 'W') robot.yLoc = robot.yLoc - 1;
+      // console.log('MOVING FORWARDS');
+      if (robot.orientation === 'E') robot.xLoc = robot.xLoc + 1;
+      if (robot.orientation === 'W') robot.xLoc = robot.xLoc - 1;
+      if (robot.orientation === 'N') robot.yLoc = robot.yLoc + 1;
+      if (robot.orientation === 'S') robot.yLoc = robot.yLoc - 1;
+
       if (
-        robot.xLoc < grid[0] ||
+        robot.xLoc < 0 ||
         robot.xLoc > grid[0] ||
-        robot.yLoc < grid[1] ||
+        robot.yLoc < 0 ||
         robot.yLoc > grid[1]
       ) {
-        // console.log('LOST');
-        robot.lost === true;
+        robot.lost = true;
       }
     }
-
-    // console.log('LOST: ', robot.lost);
 
     if (robot.lost) {
       sendMessage(robot);
       break;
     }
-
-    sendMessage(robot);
   }
+
+  sendMessage(robot);
+
+  console.log('-------');
+}
+
+function pivotRight(robot) {
+  let currentIndex = compassArray.indexOf(robot.orientation);
+  let newIndex = currentIndex + 1 > (compassArray.length - 1) ? 0 : currentIndex + 1;
+  return compassArray[newIndex];
+}
+
+function pivotLeft(robot) {
+  let currentIndex = compassArray.indexOf(robot.orientation);
+  let newIndex = currentIndex - 1 < 0 ? compassArray.length - 1 : currentIndex - 1;
+  return compassArray[newIndex];
+}
+
+function moveForwards() {
+  return value;
 }
 
 function sendMessage(robot) {
-  // console.log(`${robot.xLoc} ${robot.yLoc} ${robot.orientation} ${robot.lost ? 'LOST' : ''}`);
+  console.log(`${robot.xLoc} ${robot.yLoc} ${robot.orientation} ${robot.lost ? 'LOST' : ''}`);
 }
 
 setupMission(input);
